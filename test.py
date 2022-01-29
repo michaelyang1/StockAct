25c818dba10f3406a01824418155152c05d59d20
Seth Morley <sethmorley5@gmail.com>
2022-01-29 17:56:39 -0600

    using new api

--- test.py
+++ test.py
@@ -1,34 +1,12 @@
-
-
-from bs4 import BeautifulSoup # BeautifulSoup is in bs4 package 
-
-
 import requests
+import pandas as pd
+url = "https://api.quiverquant.com/beta/live/housetrading"
+headers = {'accept': 'application/json',
+'X-CSRFToken': 'TyTJwjuEC7VV7mOqZ622haRaaUr0x0Ng4nrwSRFKQs7vdoBcJlK9qjAS69ghzhFu',
+'Authorization': 'Token 591c8fca810b40d0893aa0276056760ed097e082'}
+r = requests.get(url, headers=headers)
+data = r.content
 
-url = "https://scrapingant.p.rapidapi.com/post"
+print(len(data))
+print(data[:100])
 
-payload = "{\n    \"cookies\": \"cookie_name_1=cookie_value_1;cookie_name_2=cookie_value_2\",\n    \"return_text\": false,\n    \"url\": \"https://app.capitoltrades.com/politician/491\"\n}"
-headers = {
-    'content-type': "application/json",
-    'x-rapidapi-host': "scrapingant.p.rapidapi.com",
-    'x-rapidapi-key': "49a69bc058msh832d1a6394dc332p11d3b1jsna43e1efd6007"
-    }
-
-response = requests.request("POST", url, data=payload, headers=headers)
-
-# print(response.text)
-
-text = response.text
-
-start = text.find('<p-table')
-stop = text.find('</p-table')+10
-
-print(start)
-print(stop)
-
-table = text[start:stop]
-
-print(table)
-print("\n\n\n")
-table = BeautifulSoup(table)
-print(table.get_text())
