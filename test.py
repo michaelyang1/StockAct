1670e5056888d251f336ae56b1f91a6e03701661
michaelyang1 <myang1394@gmail.com>
2022-01-29 18:23:55 -0600

    Integrated Quiver API and started work with REST

--- test.py
+++ test.py
@@ -1,12 +1,28 @@
+from flask import Flask, jsonify
 import requests
 import pandas as pd
-url = "https://api.quiverquant.com/beta/live/housetrading"
-headers = {'accept': 'application/json',
-'X-CSRFToken': 'TyTJwjuEC7VV7mOqZ622haRaaUr0x0Ng4nrwSRFKQs7vdoBcJlK9qjAS69ghzhFu',
-'Authorization': 'Token 591c8fca810b40d0893aa0276056760ed097e082'}
-r = requests.get(url, headers=headers)
-data = r.content
+import json
 
-print(len(data))
-print(data[:100])
+app = Flask(__name__)
 
+
+def hit_endpoint():
+    url = "https://api.quiverquant.com/beta/live/housetrading"
+    headers = {'accept': 'application/json',
+               'X-CSRFToken': 'TyTJwjuEC7VV7mOqZ622haRaaUr0x0Ng4nrwSRFKQs7vdoBcJlK9qjAS69ghzhFu',
+               'Authorization': 'Token 591c8fca810b40d0893aa0276056760ed097e082'}
+    r = requests.get(url, headers=headers)
+    data = json.loads(r.content)
+
+    for d in data[:100]:
+        print(d)
+    return data
+
+
+@app.route('/')
+def index():
+    return 'Welcome to the StockAct API'
+
+
+if __name__ == '__main__':
+    hit_endpoint()
